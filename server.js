55b59a5cd901e1dbefb8e1cda1139054e46293e7
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-12 13:58:35 -0700

    Update server boilerplate with dev logging, 500 error handler

--- server.js
+++ server.js
@@ -1,46 +1,67 @@
+'use strict';
+
+// .env file for all environmental variables
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const cors = require('cors');
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-const userRoutes        = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+// Log incoming requests in development:
+if (process.env.RUN_MODE === 'development') {
+  app.use((req, res, next) => {
+    console.log(
+      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`,
+    );
+    next();
+  });
+}
 
+// Serve static files from /public folder on any request to /public
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
+
+// Parse request JSON and url encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+// Serve index.html page on get request to '/'
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-// User routes
-userRoutes(app);
-    
-// 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+// Routing for API
+apiRoutes(app);
+
+// 404 page not found:
+app.get('*', (req, res) => {
+  // Redirect to index
+  res.redirect('/');
+});
+
+// Internal Error Handler:
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Internal Server error: See Server Logs');
 });
 
 const portNum = process.env.PORT || 3000;
 
 // Start our server and tests!
+// http://localhost:3000
 app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
