f8a60ba7ff99eeab669049b8baa5354a996e37e7
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-12 13:59:11 -0700

    Added explanatory comments to translation dictionaries in components/

--- components/american-to-british-titles.js
+++ components/american-to-british-titles.js
@@ -1,8 +1,10 @@
+// Translation of American titles to British equivalents
+
 module.exports = {
   'mr.': 'mr',
   'mrs.': 'mrs',
   'ms.': 'ms',
   'mx.': 'mx',
   'dr.': 'dr',
-  'prof.': 'prof'
-}
+  'prof.': 'prof',
+};
